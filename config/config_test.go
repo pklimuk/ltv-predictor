d99a4ef0c45e4de8a79299966e106a8cc83df181
Pavel Klimuk <pavelklimuk@outlook.com>
2023-09-23 19:26:48 +0200

    Added tests for config package

---
+++ config/config_test.go
@@ -0,0 +1,220 @@
+package config
+
+import (
+	"errors"
+	"testing"
+
+	"github.com/pklimuk/ltv-predictor/aggregator"
+	"github.com/pklimuk/ltv-predictor/fileParser"
+	"github.com/pklimuk/ltv-predictor/flagsParser"
+	"github.com/pklimuk/ltv-predictor/outputPrinter"
+	"github.com/pklimuk/ltv-predictor/predictor"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestCreateAppConfig(t *testing.T) {
+	tests := []struct {
+		name           string
+		flags          *flagsParser.Flags
+		expectedConfig *AppConfig
+		expectedErr    error
+	}{
+		{
+			name: "Valid config",
+			flags: &flagsParser.Flags{
+				Source:           "data.csv",
+				AggregateBy:      "country",
+				Model:            "linearExtrapolation",
+				PredictionLength: 10,
+			},
+			expectedConfig: &AppConfig{
+				Parser:           fileParser.CSVParser{Path: "data.csv"},
+				Aggregator:       aggregator.ByCountryAggregator{},
+				Predictor:        predictor.LinearExtrapolator{},
+				OutputPrinter:    outputPrinter.ConsolePrinter{},
+				PredictionLength: 10,
+			},
+			expectedErr: nil,
+		},
+		{
+			name: "Invalid prediction length",
+			flags: &flagsParser.Flags{
+				Source:           "data.csv",
+				AggregateBy:      "country",
+				Model:            "linearExtrapolation",
+				PredictionLength: -1,
+			},
+			expectedConfig: nil,
+			expectedErr:    errors.New(ErrPredictionLengthNotPositive),
+		},
+		{
+			name: "Unknown model",
+			flags: &flagsParser.Flags{
+				Source:           "data.csv",
+				AggregateBy:      "country",
+				Model:            "invalidModel",
+				PredictionLength: 10,
+			},
+			expectedConfig: nil,
+			expectedErr:    errors.New(ErrUnknownModel),
+		},
+		{
+			name: "Invalid file format",
+			flags: &flagsParser.Flags{
+				Source:           "data.txt",
+				AggregateBy:      "country",
+				Model:            "linearExtrapolation",
+				PredictionLength: 10,
+			},
+			expectedConfig: nil,
+			expectedErr:    errors.New(ErrUnsupportedFileFormat),
+		},
+		{
+			name: "Invalid aggregation field",
+			flags: &flagsParser.Flags{
+				Source:           "data.csv",
+				AggregateBy:      "invalidField",
+				Model:            "linearExtrapolation",
+				PredictionLength: 10,
+			},
+			expectedConfig: nil,
+			expectedErr:    errors.New(ErrUnknownAggregateBy),
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			config, err := CreateAppConfig(test.flags)
+
+			if test.expectedErr != nil {
+				assert.Error(t, err)
+				assert.Nil(t, config)
+				assert.EqualError(t, err, test.expectedErr.Error())
+			} else {
+				assert.NoError(t, err)
+				assert.NotNil(t, config)
+				assert.Equal(t, test.expectedConfig.Parser, config.Parser)
+				assert.Equal(t, test.expectedConfig.Aggregator, config.Aggregator)
+				assert.Equal(t, test.expectedConfig.Predictor, config.Predictor)
+				assert.Equal(t, test.expectedConfig.OutputPrinter, config.OutputPrinter)
+				assert.Equal(t, test.expectedConfig.PredictionLength, config.PredictionLength)
+			}
+		})
+	}
+}
+
+func TestCreateParser(t *testing.T) {
+	tests := []struct {
+		name         string
+		source       string
+		expectedType fileParser.FileParser
+		expectedErr  error
+	}{
+		{"CSV file", "file.csv", fileParser.CSVParser{}, nil},
+		{"JSON file", "file.json", fileParser.JSONParser{}, nil},
+		{"Unknown file format", "file.txt", nil, errors.New(ErrUnsupportedFileFormat)},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			flags := &flagsParser.Flags{Source: test.source}
+			parser, err := createParser(flags)
+
+			if test.expectedErr != nil {
+				assert.Error(t, err)
+				assert.Nil(t, parser)
+				assert.EqualError(t, err, test.expectedErr.Error())
+			} else {
+				assert.NoError(t, err)
+				assert.NotNil(t, parser)
+				assert.IsType(t, test.expectedType, parser)
+			}
+		})
+	}
+}
+
+func TestCreateAggregator(t *testing.T) {
+	tests := []struct {
+		name         string
+		aggregateBy  string
+		expectedType aggregator.Aggregator
+		expectedErr  error
+	}{
+		{"Country aggregator", "country", aggregator.ByCountryAggregator{}, nil},
+		{"Campaign aggregator", "campaign", aggregator.ByCampaignAggregator{}, nil},
+		{"Unknown aggregator", "unknown", nil, errors.New(ErrUnknownAggregateBy)},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			flags := &flagsParser.Flags{AggregateBy: test.aggregateBy}
+			aggregator, err := createAggregator(flags)
+
+			if test.expectedErr != nil {
+				assert.Error(t, err)
+				assert.Nil(t, aggregator)
+				assert.EqualError(t, err, test.expectedErr.Error())
+			} else {
+				assert.NoError(t, err)
+				assert.NotNil(t, aggregator)
+				assert.IsType(t, test.expectedType, aggregator)
+			}
+		})
+	}
+}
+
+func TestCreatePredictor(t *testing.T) {
+	tests := []struct {
+		name         string
+		model        string
+		expectedType predictor.Predictor
+		expectedErr  error
+	}{
+		{"Linear extrapolation", "linearExtrapolation", predictor.LinearExtrapolator{}, nil},
+		{"Linear regression", "linearRegression", predictor.LinearRegressor{}, nil},
+		{"Unknown model", "unknown", nil, errors.New(ErrUnknownModel)},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			flags := &flagsParser.Flags{Model: test.model}
+			predictor, err := createPredictor(flags)
+
+			if test.expectedErr != nil {
+				assert.Error(t, err)
+				assert.Nil(t, predictor)
+				assert.EqualError(t, err, test.expectedErr.Error())
+			} else {
+				assert.NoError(t, err)
+				assert.NotNil(t, predictor)
+				assert.IsType(t, test.expectedType, predictor)
+			}
+		})
+	}
+}
+
+func TestValidatePredictionLength(t *testing.T) {
+	tests := []struct {
+		name             string
+		predictionLength int64
+		expectedErr      error
+	}{
+		{"Negative prediction length", -1, errors.New(ErrPredictionLengthNotPositive)},
+		{"Zero prediction length", 0, errors.New(ErrPredictionLengthNotPositive)},
+		{"Too short prediction length", 7, errors.New(ErrPredictionLengthTooShort)},
+		{"Valid prediction length", 10, nil},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			err := validatePredictionLength(test.predictionLength)
+
+			if test.expectedErr != nil {
+				assert.Error(t, err)
+				assert.EqualError(t, err, test.expectedErr.Error())
+			} else {
+				assert.NoError(t, err)
+			}
+		})
+	}
+}
